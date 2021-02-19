7baf0d5c6dc58e393fd49e79e9913da42bb70e82
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-02-19 22:36:05 +0700

    done make responsive register form

---
+++ src/components/FormRegister.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+
+const Form = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [company, setCompany] = useState('')
+  const [category, setCategory] = useState('')
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleCompany = (e) => {
+    setCompany(e.target.value)
+  }
+
+  const handleCategory = (e) => {
+    setCategory(e.target.value)
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    let payload = {
+      username,
+      password
+    }
+    console.log(payload);
+  }
+
+  return (
+    <div className="max-w-md mx-auto justify-center p-6 flex bg-gray-100 mt-10 rounded shadow-xl">
+          <form className='w-full' action='' onSubmit={handleLogin}>
+              <div className='flex flex-col w-full text-left mb-4'>
+                <label className='form-text mr-2 font-bold text-lg' for="username">Username</label>
+                <input className='border w-full border-blue-400 rounded-md py-2 px-3 text-grey-darknest' onChange={handleUsername} type="text" name="username" id="username" />
+              </div>
+              <div className='flex flex-col text-left mb-4'>
+                <label className='form-text mr-3 font-bold text-lg' for="password">Password</label>
+                <input className='border w-full border-blue-400 rounded-md py-2 px-3 text-grey-darknest' onChange={handlePassword} type="password" name="password" id="password" />
+              </div>
+              <div className='flex flex-col w-full text-left mb-4'>
+                <label className='form-text mr-2 font-bold text-lg' for="email">Email</label>
+                <input className='border w-full border-blue-400 rounded-md py-2 px-3 text-grey-darknest' onChange={handleEmail} type="text" name="email" id="email" />
+              </div>
+              <div className='flex flex-col w-full text-left mb-4'>
+                <label className='form-text mr-2 font-bold text-lg' for="company">Company Name</label>
+                <input className='border border-blue-400 rounded-md py-2 px-3 text-grey-darknest' onChange={handleCompany} type="text" name="company" id="company" />
+              </div>
+              <div className='flex flex-col w-full text-left mb-4'>
+                <label className='form-text mr-2 font-bold text-lg' for="category">category</label>
+                <input className='border w-full border-blue-400 rounded-md py-2 px-3 text-grey-darknest' onChange={handleCategory} type="text" name="category" id="category" />
+              </div>
+              <button className='button-form p-2 rounded-lg' type="submit">Register</button>
+          </form>
+    </div>
+  );
+};
+
+export default Form;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
         </div>
       </div>
     </div>
-    
+
     <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
       <div className="ml-6 pt-1">
         <h1 className="text-2xl text-blue-700 leading-tight">
